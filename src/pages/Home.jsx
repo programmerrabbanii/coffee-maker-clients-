5dbee6b25be009a1d4a9cac67d1aa4fa06dad027
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-11-27 12:22:43 +0600

    added the silver

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import BannarBottom from "../components/BannarBottom";
 import CoffeeCard from "../components/CoffeeCard";
 import { useState } from "react";
 import Gallary from "../components/Gallary";
+import { FaCoffee } from "react-icons/fa";
 
 const Home = () => {
   const loadedCoffees = useLoaderData();
@@ -17,13 +18,18 @@ const Home = () => {
         </h3>
         <p className="text-lg text-gray-600 mt-2">Our Popular Products</p>
 
-        <Link
-          to="/addcoffe"
-          className="inline-flex items-center gap-2 px-6 py-3 mt-6 bg-[#c2a478] text-white font-semibold text-lg rounded-md hover:bg-[#a7865b] transition focus:outline-none focus:ring-2 focus:ring-[#c2a478] focus:ring-offset-2"
-        >
-          <span>Add Coffee</span>
-          <i className="fas fa-coffee text-lg"></i>
-        </Link>
+        
+  
+
+  <Link
+        to="/addcoffe"
+        className="inline-flex items-center gap-2 px-6 py-3 mt-6 bg-[#c2a478] text-white font-semibold text-lg rounded-md hover:bg-[#a7865b] transition focus:outline-none focus:ring-2 focus:ring-[#c2a478] focus:ring-offset-2"
+      >
+        <span>Add Coffee</span>
+        <FaCoffee className="text-xl" />
+      </Link>
+
+ 
       </div> 
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-10 w-11/12 mx-auto">
