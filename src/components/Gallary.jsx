18ce639ca449c54e0b70f6d8c58b53f3ed94d523
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-11-27 12:29:50 +0600

    added the  gallary secen part

--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -10,12 +10,12 @@ import coffee8 from '../assets/images/cups/Rectangle 16.png'
 
 const Gallary = () => {
     return (
-        <div className='mt-10 w-11/12 mx-auto mb-10'>
+        <div className='mt-10 w-11/12 mx-auto mb-12'>
             <h3 className='text-center text-3xl font-semibold '>See Our New Coffees Collection Here Bara..</h3>
             <p className='text-center mb-8'>If you have a girlfriend, you get a 50% discount on this bara.. </p>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
                 <div>
-                   <img src={coffee1} alt="" />
+                   <img src={coffee1} alt="" /> 
                 </div>
                 <div> 
                    <img src={coffee2} alt="" />
