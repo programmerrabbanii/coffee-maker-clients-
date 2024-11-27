a0640e785d5fa36910613679b361115b2f33757d
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-11-27 12:28:56 +0600

    added the pragraph sectin

--- src/components/Bannar.jsx
+++ src/components/Bannar.jsx
@@ -13,14 +13,14 @@ const Bannar = () => {
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content flex items-center justify-end w-full h-full"> {/* Flex container */}
-          <div className=" w-8/12 text-right mr-0 px-32"> {/* Right aligned content */}
+        <div className="hero-content text-neutral-content flex items-center justify-end w-full h-full"> 
+          <div className=" w-8/12 text-right mr-0 px-32"> 
             <h1 className="mb-5 text-5xl font-bold">
               Would you like a Cup of Delicious Coffee?   
 
             </h1> 
             <p className="mb-5">
-              It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.
+              It is coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.
             </p>
             <Link to='/addcoffe'><button className="btn btn-primary ">Add Coffee</button>
             </Link>
