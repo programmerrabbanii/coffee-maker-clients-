f78a2e0cfa581ebcb68705085ebd4699321b02c0
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-11-27 12:50:00 +0600

    added the favicon icone

--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -16,7 +16,7 @@ const Gallary = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
                 <div>
                    <img src={coffee1} alt="" /> 
-                </div>
+                </div> 
                 <div> 
                    <img src={coffee2} alt="" />
                 </div>
